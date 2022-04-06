aba29d70f2f5fc050740f5aca3b99a5ae1a43366
Thiago Jose <thiago02_08@hotmail.com>
2022-04-06 16:04:56 -0300

    salvando mentor novo no console

--- FrontEnd-React/technical-share-front/src/components/AddMentoresComponent.js
+++ FrontEnd-React/technical-share-front/src/components/AddMentoresComponent.js
@@ -1,9 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddMentoresComponent = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [emailId, setEmailId] = useState("");
+
+  const saveMentores = (e) => {
+    e.preventDefault();
+
+    const mentores = { firstName, lastName, emailId };
+
+    console.log(mentores);
+  };
+
   return (
     <div>
-      <h1> Componente Adicionar Mentor </h1>
+      <br /> <br />
+      <div className="container">
+        <div className="row">
+          <div className="card col-md-6 offset-md-3 offset-md-3">
+            <h2 className="text-center"> Adicionar Mentor </h2>
+            <div className="card-body">
+              <form>
+                <div className="form-group mb-2">
+                  <label className="form-label">Primeiro Nome</label>
+                  <input
+                    type="text"
+                    placeholder="Digite o primeiro nome"
+                    name="firstName"
+                    className="form-control"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                  ></input>
+                </div>
+                <div className="form-group mb-2">
+                  <label className="form-label"> Sobrenome</label>
+                  <input
+                    type="text"
+                    placeholder="Digite o Sobrenome"
+                    name="lastName"
+                    className="form-control"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                  ></input>
+                </div>
+                <div className="form-group mb-2">
+                  <label className="form-label">Email</label>
+                  <input
+                    type="email"
+                    placeholder="Digite o email"
+                    name="emailId"
+                    className="form-control"
+                    value={emailId}
+                    onChange={(e) => setEmailId(e.target.value)}
+                  ></input>
+                </div>
+
+                <button
+                  className="btn btn-success"
+                  onClick={(e) => saveMentores(e)}
+                >
+                  Registrar
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

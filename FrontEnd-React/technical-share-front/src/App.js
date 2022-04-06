6fe5e300c9876d288ca75b3d727302142fbc74a4
Thiago Jose <thiago02_08@hotmail.com>
2022-04-06 10:39:33 -0300

    cria o header e o footer

--- FrontEnd-React/technical-share-front/src/App.js
+++ FrontEnd-React/technical-share-front/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import React from "react";
 import ListMentoresComponent from "./components/ListMentoresComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
 
 function App() {
   return (
     <div>
+      <HeaderComponent />
       <ListMentoresComponent />
+      <FooterComponent />
     </div>
   );
 }
---
+++ FrontEnd-React/technical-share-front/src/components/FooterComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const FooterComponent = () => {
+  return (
+    <div>
+      <footer className="footer">
+        <span className="text-muted">
+          Orgulhosamente desenvolvido pelo Squad 29
+        </span>
+      </footer>
+    </div>
+  );
+};
+
+export default FooterComponent;

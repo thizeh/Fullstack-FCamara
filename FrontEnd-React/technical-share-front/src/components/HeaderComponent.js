8771332da9098140916e0f2316b359d1a7fe1bb9
Thiago Jose <thiago02_08@hotmail.com>
2022-04-07 19:18:24 -0300

    teste funcional

--- FrontEnd-React/technical-share-front/src/components/HeaderComponent.js
+++ FrontEnd-React/technical-share-front/src/components/HeaderComponent.js
@@ -6,7 +6,10 @@ const HeaderComponent = () => {
       <header>
         <nav className="navbar navbar-expand-md navbar-dark bg-dark">
           <div>
-            <a href="http://localhost:3000/" className="navbar-brand">
+            <a
+              href="https://fullstack-f-camara.vercel.app/"
+              className="navbar-brand"
+            >
               Technical Share
             </a>
           </div>

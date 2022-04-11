e71b29635af8a2c710db305b60cde02459411ce9
Thiago Jose <thiago02_08@hotmail.com>
2022-04-11 09:21:24 -0300

    heroku db

--- FrontEnd-React/technical-share-front/src/services/MentoresService.js
+++ FrontEnd-React/technical-share-front/src/services/MentoresService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const MENTORES_BASE_REST_API_URL = "http://localhost:8080/api/v1/mentores";
+const MENTORES_BASE_REST_API_URL =
+  "http://fc29.herokuapp.com/codejava/api/v1/mentores";
 
 class MentoresService {
   getAllMentores() {
